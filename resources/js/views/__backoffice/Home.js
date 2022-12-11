26a52de8e9490588160682f7114a018a2dd49d03
AlbertPF <110432856+AlbertPF@users.noreply.github.com>
2022-12-10 19:14:17 -0500

    Agregando un card a Home

--- resources/js/views/__backoffice/Home.js
+++ resources/js/views/__backoffice/Home.js
@@ -3,7 +3,36 @@ import { Typography } from '@material-ui/core';
 
 import { Master as MasterLayout } from './layouts';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+//import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  };
+
 function Home(props) {
+
+    const { classes } = props;
+    const bull = <span className={classes.bullet}>•</span>;
+
     const primaryAction = {
         text: 'Export Stats',
         clicked: () => alert('Exporting your awesome stats...'),
@@ -21,6 +50,7 @@ function Home(props) {
         },
     ];
 
+
     return (
         <MasterLayout
             {...props}
@@ -29,8 +59,31 @@ function Home(props) {
             tabs={tabs}
         >
             <Typography>There is no place like home</Typography>
+
+            <Card >
+                <CardContent>
+                    <Typography variant="h5"  component="h2"  color="textSecondary" gutterBottom>
+                    Word of the Day
+                    </Typography>
+                    <Typography component="p">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained" color="primary" onClick="#">
+                        Iniciar un Pago!
+                    </Button>
+                </CardActions>
+            </Card>
+
         </MasterLayout>
     );
 }
 
-export default Home;
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(Home);
