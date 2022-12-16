5e657f46b1b32c84dcd4db151a81e74c4912837b
Velnae <nahuinlla101131@gmail.com>
2022-12-16 11:32:38 -0500

    personalizo seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,32 +14,62 @@ class UsersTableSeeder extends Seeder
     {
         $user = new User;
         $user->type = 'superuser';
-        $user->name = 'Jovert Palonpon';
-        $user->username = 'jovert123';
-        $user->email = 'jovert@example.com';
-        $user->password = bcrypt('secret');
+        $user->name = 'Emerson';
+        $user->username = 'administrador';
+        $user->email = 'nahuinlla101131@example.com';
+        $user->password = bcrypt('123456');
 
-        $user->firstname = 'Jovert';
-        $user->middlename = 'Lota';
-        $user->lastname = 'Palonpon';
+        $user->firstname = 'Emerson';
+        $user->middlename = null;
+        $user->lastname = 'Ñahuinlla Velásquez';
         $user->gender = 'male';
-        $user->birthdate = '1998-05-18';
-        $user->address = 'Marungko, Angat, Bulacan';
+        $user->birthdate = '1993-07-25';
+        $user->address = 'Av Canada SN Abancay Abancay Apurímac';
         $user->save();
 
         $user = new User;
-        $user->type = 'superuser';
-        $user->name = 'Ian Lumbao';
-        $user->username = 'ian123';
-        $user->email = 'ian@example.com';
-        $user->password = bcrypt('secret');
+        $user->type = 'user';
+        $user->name = 'Zeilu';
+        $user->username = 'zeilu';
+        $user->email = 'zeilu@gmail.com';
+        $user->password = bcrypt('123456');
 
-        $user->firstname = 'Ian';
-        $user->middlename = null;
-        $user->lastname = 'Lumbao';
+        $user->firstname = 'Zeilu';
+        $user->middlename = 'Massiel';
+        $user->lastname = 'Moreano Falcon';
+        $user->gender = 'female';
+        $user->birthdate = null;
+        $user->address = 'Av panamericana SN Abancay Abancay Apurímac';
+        $user->save();
+
+        $user = new User;
+        $user->type = 'user';
+        $user->name = 'Luis';
+        $user->username = 'Luis';
+        $user->email = 'luis@gmail.com';
+        $user->password = bcrypt('123456');
+
+        $user->firstname = 'luis';
+        $user->middlename = 'Alberto';
+        $user->lastname = 'Pfuño Alccahuamani';
         $user->gender = 'male';
         $user->birthdate = null;
-        $user->address = 'Tayuman, Manila, Metro Manila';
+        $user->address = 'Av panamericana SN Abancay Abancay Apurímac';
+        $user->save();
+
+        $user = new User;
+        $user->type = 'user';
+        $user->name = 'Jhon';
+        $user->username = 'Jhon';
+        $user->email = 'Jhon@gmail.com';
+        $user->password = bcrypt('123456');
+
+        $user->firstname = 'John';
+        $user->middlename = 'Franklin';
+        $user->lastname = 'Romero Ramos';
+        $user->gender = 'male';
+        $user->birthdate = null;
+        $user->address = 'Av panamericana SN Abancay Abancay Apurímac';
         $user->save();
     }
 }
